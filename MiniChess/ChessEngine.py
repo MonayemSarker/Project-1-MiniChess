e079926ae1b9899ec0fbf7828b67bbadf7cb37ba
MonayemSarker <bsse1228@iit.du.ac.bd>
2023-09-03 01:01:13 +0600

    Move added
    the pieces can move but not implemented logical moves yet

--- MiniChess/ChessEngine.py
+++ MiniChess/ChessEngine.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class GameState():
     def __init__(self):
         self.board = np.array([
@@ -13,3 +14,29 @@ class GameState():
 
         self.whiteToMove = True
         self.moveLog = []
+
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "--"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.moveLog.append(move)
+        self.whiteToMove = not self.whiteToMove
+class Move():
+    ranksToRows = {"1": 5, "2": 4, "3": 3, "4": 2, "5": 1, "6": 0}
+    rowsToRanks = {v: k for k, v in ranksToRows.items()}
+
+    filesToCols = {"1": 4, "2": 3, "3": 2, "4": 1, "5": 0}
+    colsToFiles = {v: k for k, v in filesToCols.items()}
+
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.getRankFile(self.startRow, self.startCol) + self.getRankFile(self.endRow, self.endCol)
+
+    def getRankFile(self, r, c):
+        return self.colsToFiles[c] + self.rowsToRanks[r]
